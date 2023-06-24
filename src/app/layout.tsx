4e7a222bee29408e59c9c3089c9a76834b653f9c
Mateus Moraes <cmateusmoraes@gmail.com>
2023-06-24 17:11:41 -0300

    Otimizacao de imagens e textos para opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,25 @@ import { Footer } from "@/components/Footer";
 const space = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "MateusMoraes - Desenvolvedor Front-End",
+  title: "Mateus Moraes - Desenvolvedor Front-End",
   description:
-    "Desenvolvedor especialista front-end com mais de 20 anos de experiência.",
+    "Desenvolvedor especialista front-end com mais de 20 anos de experiência | React, Javascript, Next.js, TypescriptHTML, CSS ",
+  openGraph: {
+    title: "Mateus Moraes - Desenvolvedor Front-End",
+    description:
+      "Desenvolvedor especialista front-end com mais de 20 anos de experiência | React, Javascript, Next.js, Typescript HTML, CSS ",
+    url: "https://${process.env.VERCEL_URL}",
+    siteName: "Mateus Moraes - Portfolio",
+    images: [
+      {
+        url: "https://${process.env.VERCEL_URL}/image/jobs/portfolio.jpg",
+        width: 800,
+        height: 546,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
